ff06c769524f5e6b26d5baaf04883cec7d05a3f7
Jason Ronallo <jronallo@gmail.com>
2011-02-28 11:24:27 -0500

    update readme. fix bug in initializer where parent tmp directory would not get created

--- config/initializers/locker.rb
+++ config/initializers/locker.rb
@@ -1,3 +1,3 @@
 [:locker, :tank, :repository_pairtree, :access_pairtree].each do |key|
-  Dir.mkdir(APP_CONFIG[key]) unless File.exists?(APP_CONFIG[key])
+  FileUtils.mkdir_p(APP_CONFIG[key]) unless File.exists?(APP_CONFIG[key])
 end
